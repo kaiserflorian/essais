3106c509e878e01b2c00d774893b3c179f8604df
kaiserflorian <kaiserflorian00@gmail.com>
2013-11-09 14:34:19 +0100

    Done

--- nombres.py
+++ nombres.py
@@ -1,10 +1,4 @@
 __author__ = 'kaiserfl'
-total =0
-for i in range(1,100,1):
-    print("J'ajoute " + str(i) + " au total de " + str(total))
-    total = total + i
-    if total>100:
-        print("J'en ai marre...")
-        break
 
-print(total)
+for i in range(1,11):
+   print(i)
