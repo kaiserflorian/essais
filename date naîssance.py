5b3b4cab5b3448af6b5f0c9bf2541d846c8a5c1c
kaiserflorian <kaiserflorian00@gmail.com>
2013-11-09 12:03:47 +0100

    Petite modif

--- date naîssance.py
+++ date naîssance.py
@@ -14,6 +14,10 @@ calcul = annee_courante - int(reponse)
 
 if calcul >= 0:
     print("Vous avez environ " + str(calcul) + " ans")
+    if calcul > 105:
+        print("Ouhlala, vous êtes en très bonne santé...")
+    if calcul > 200:
+        print("Euh tu n'est pas en train de tricher???????")
 else:
     print("Tu rigoles, t'es même pas encore né!")
 
